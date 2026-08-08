68199690bab80f7ec2deb37a5663906f024e6fec
Suvra05 <suvrajitdas2005@gmail.com>
2026-08-09 00:48:41 +0530

    h1 and h2 changes, Switch to SPA mode for static development(ssr:false)

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -52,7 +52,7 @@ const Resume = () => {
 
     return (
         <main className="!pt-0">
-            <div>Resume {id}</div>
+            {/*<div>Resume {id}</div>*/}
             <nav className="resume-nav">
                 <Link to="/" className="back-button">
                     <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
